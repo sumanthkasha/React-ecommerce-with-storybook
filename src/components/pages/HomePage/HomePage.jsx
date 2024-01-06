eccb0da5139de925aaea9004482a69400628fc2a
kasha.sumanth <kasha.sumanth@tadigital.com>
2024-01-07 00:27:34 +0530

    Added carousel to storybook

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -2,12 +2,10 @@ import { HeroBanner } from '../../../stories/organisms/HeroBanner/HeroBanner';
 import './HomePage.scss';
 
 export default function Home() {
+
     return (
         <main>
             <HeroBanner />
-            {/* <div>
-                Home Page
-            </div> */}
         </main>
     )
 }
