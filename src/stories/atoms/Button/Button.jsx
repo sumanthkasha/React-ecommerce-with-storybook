491898875076bf484b82ff28e762ee2b6e4f78f3
kasha.sumanth <kasha.sumanth@tadigital.com>
2024-01-06 10:34:20 +0530

    Updates in Header component

--- src/stories/atoms/Button/Button.jsx
+++ src/stories/atoms/Button/Button.jsx
@@ -6,14 +6,14 @@ import './button.scss';
  * Primary UI component for user interaction
  */
 
-export const Button = ({ primary, backgroundColor, size, label, onClick, ...props }) => {
+export const Button = ({ primary, backgroundColor, size, label, type="button", onClick, ...props }) => {
   
   
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
 
   return (
     <button
-      type="button"
+      type={type}
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
       style={backgroundColor && { backgroundColor }}
       onClick={onClick}
