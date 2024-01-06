27b590debea45a6258a9d1ffb7350fc975a1b2ee
kasha.sumanth <kasha.sumanth@tadigital.com>
2024-01-07 01:17:12 +0530

    Created Error page

---
+++ src/stories/pages/Error/Error.stories.js
@@ -0,0 +1,13 @@
+import { Error } from "./Error";
+
+export default {
+    title: 'Pages/ErrorPage',
+    component: Error,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+}
+
+export const ErrorCmp = {
+};
