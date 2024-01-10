c5da002826818494cebfea5120e8a8058c93c29b
kasha.sumanth <kasha.sumanth@tadigital.com>
2024-01-10 14:15:28 +0530

    fixed issues in wishlist

--- src/components/pages/CollectionsPage/CollectionsPage.jsx
+++ src/components/pages/CollectionsPage/CollectionsPage.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addProduct, removeProduct } from "../../../redux/slice/productDataSlice";
 
@@ -11,9 +10,9 @@ import "./CollectionsPage.scss";
 
 const CollectionsPage = () => {
     const dispatch = useDispatch();
-    const state = useSelector((state) => state);
+    const { isLoading, data } = useSelector((state) => state.productData);
 
-    const [productList, setProductList] = useState(state.productData.data || []);
+    const [productList, setProductList] = useState(data || []);
     const [filteredProductList, setFilteredProductList] = useState(null);
     const [activeCategory, setActiveCategory] = useState(null);
     const [activeSortByPrice, setActiveSortByPrice] = useState(null);
@@ -21,10 +20,11 @@ const CollectionsPage = () => {
     const [wishlistData, setWishListData] = useState({});
 
     useEffect(() => {
-        if(productList != null) {
-            filteredProducts("tshirt")
+        if (data) {
+            setProductList(data);
+            filteredProducts("tshirt");
         }
-    }, [productList]);
+    }, [data]);
 
     useEffect(() => {
         for (const productId in wishlistData) {
@@ -40,33 +40,29 @@ const CollectionsPage = () => {
     }, [wishlistData, dispatch]);
 
     const handleUpdateWishlist = (productId) => {
-        const newStatus = wishlistData[productId] === "add" ? "remove" : "add";
-        setWishListData({
-          ...wishlistData,
-          [productId]: newStatus,
+        setWishListData((prevWishlistData) => {
+            const newStatus = prevWishlistData[productId] === "add" ? "remove" : "add";
+            return {
+                ...prevWishlistData,
+                [productId]: newStatus,
+            };
         });
     };
 
-    const filteredProducts = (data="tshirt") => {
-        const renderedProducts = [];
+    console.log(useSelector((state) => state));
 
-        productList.map((product) => {
-            if(product.type == data) {
-                renderedProducts.push(product);
-            }
-        });
+    const filteredProducts = (data = "tshirt") => {
+        const renderedProducts = productList.filter((product) => product.type === data);
         setFilteredProductList(renderedProducts);
         setActiveCategory(data);
+    };
+
+    if (isLoading) {
+        return <Loader />;
     }
 
-    if (state.productData.isLoading) {
-        return (
-            <Loader />
-        )
-    }
-
-    const filterCategories = (originalItems) => {
-        const categoryCounts = originalItems.reduce((counts, item) => {
+    const filterCategories = () => {
+        const categoryCounts = productList.reduce((counts, item) => {
             const brandName = item.type;
             if (brandName !== undefined) {
                 counts[brandName] = (counts[brandName] || 0) + 1;
@@ -74,88 +70,72 @@ const CollectionsPage = () => {
             return counts;
         }, {});
 
-        const categoryListItems = [];
-
-        for (const brandName in categoryCounts) {
-            if (categoryCounts.hasOwnProperty(brandName)) {
-                categoryListItems.push(
-                    <li key={brandName} className={activeCategory == brandName ? 'active' : ''} onClick={() => filteredProducts(brandName)}>
-                        <span>{`${brandName+"s"}`}</span>
-                        {/* <span>{`${categoryCounts[brandName]}`}</span> */}
-                    </li>
-                );
-            }
-        }
-        return categoryListItems;
-    }
+        return Object.entries(categoryCounts).map(([brandName, count]) => (
+            <li key={brandName} className={activeCategory === brandName ? 'active' : ''} onClick={() => filteredProducts(brandName)}>
+                <span>{`${brandName}s`}</span>
+            </li>
+        ));
+    };
 
     const sortByPrice = () => {
         const sortedList = [...filteredProductList];
-        sortedList.sort((prod1, prod2) => (
-            prod1.price > prod2.price ? 1 : -1
-        ));
+        sortedList.sort((prod1, prod2) => (prod1.price > prod2.price ? 1 : -1));
         setFilteredProductList(sortedList);
         setActiveSortByPrice(true);
         setActiveSortByDiscount(false);
-    }
+    };
 
-    const sortBydiscount = () => {
+    const sortByDiscount = () => {
         const sortedList = [...filteredProductList];
-        sortedList.sort((prod1, prod2) => (
-            prod1.discount > prod2.discount ? 1 : -1
-        ));
+        sortedList.sort((prod1, prod2) => (prod1.discount > prod2.discount ? 1 : -1));
         setFilteredProductList(sortedList);
         setActiveSortByPrice(false);
         setActiveSortByDiscount(true);
-    }
+    };
 
     return (
         <div className="d-flex flex-column px-2">
             <section className="d-flex align-items-center product__categories">
                 <ol className="d-flex align-items-center product__filters mb-0">
-                    {
-                        productList && filterCategories(productList)
-                    }
+                    {productList && filterCategories()}
                 </ol>
                 <section className="product__sorts d-flex justify-content-end ms-auto">
-                    <span className={activeSortByPrice == true ? "product__sort-by-price active" : "product__sort-by-price"} onClick={() => sortByPrice()}>sort-by-price</span>
-                    <span className={activeSortByDiscount == true ? "product__sort-by-discount active" : "product__sort-by-discount"} onClick={() => sortBydiscount()}>sort-by-discount</span>
+                    <span className={activeSortByPrice ? "product__sort-by-price active" : "product__sort-by-price"} onClick={sortByPrice}>
+                        sort-by-price
+                    </span>
+                    <span className={activeSortByDiscount ? "product__sort-by-discount active" : "product__sort-by-discount"} onClick={sortByDiscount}>
+                        sort-by-discount
+                    </span>
                 </section>
             </section>
             <main className="product__filtered">
                 <div>
                     <section id="filteredProducts">
                         <ul className="product__product-list">
-                            { 
-                                filteredProductList && filteredProductList.map((prod) => (
-
+                            {filteredProductList &&
+                                filteredProductList.map((prod) => (
                                     <li key={prod.id} className="d-flex flex-column product__product">
                                         <Button
                                             className="product__wishlist"
                                             value={prod.id}
                                             onClick={() => handleUpdateWishlist(prod.id)}
                                         >
-                                            {
-                                                wishlistData[prod.id.toString()] === 'remove' || !wishlistData[prod.id]
-                                                ? <CiHeart />
-                                                : <FaHeart />
-                                            }
+                                            {wishlistData[prod.id.toString()] === 'remove' || !wishlistData[prod.id] ? <CiHeart /> : <FaHeart />}
                                         </Button>
-                                        <img className="product__img" src={ "./images/"+prod.image } alt={ prod.brand_name + " " + prod.type } />
-                                        <span className="product__brand"> { prod.brand_name } </span>
-                                        <span className="product__desc"> { prod.description } </span>
-                                        <span className="product__discount"> { prod.discount } </span>
-                                        <span className="product__popularity"> { prod.popularity } </span>
-                                        <span className="product__price"> { prod.price } </span>
+                                        <img className="product__img" src={"./images/" + prod.image} alt={prod.brand_name + " " + prod.type} />
+                                        <span className="product__brand"> {prod.brand_name} </span>
+                                        <span className="product__desc"> {prod.description} </span>
+                                        <span className="product__discount"> {prod.discount} </span>
+                                        <span className="product__popularity"> {prod.popularity} </span>
+                                        <span className="product__price"> {prod.price} </span>
                                     </li>
-                                ))
-                            }
+                                ))}
                         </ul>
                     </section>
                 </div>
             </main>
         </div>
-    )
+    );
 };
 
 export default CollectionsPage;
